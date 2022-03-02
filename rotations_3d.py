f605e6d31e676c308266550caf189044d720739d
israel-iap <israel.iap@outlook.com>
2022-03-02 02:42:04 -0600

    create rotations_3d

---
+++ rotations_3d.py
@@ -0,0 +1,12 @@
+import math as math
+import numpy as np
+
+atd = np.matrix([[1, 0, 0, -1],
+                 [0, 1, 0, 1],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 1]])
+
+ctd = np.matrix([[0, 1, 0, -1],
+                 [0, 1, 0, 1],
+                 [0, 0, 1, 0],
+                 [0, 0, 0, 1]])
