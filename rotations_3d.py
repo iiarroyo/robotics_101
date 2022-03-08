ab987240ffeba5e22912c0d20ffe59368d721de6
israel-iap <israel.iap@outlook.com>
2022-03-08 00:11:54 -0600

    Create programming assignment for FK

--- rotations_3d.py
+++ rotations_3d.py
@@ -1,12 +1,22 @@
 import math as math
 import numpy as np
+from numpy.linalg import inv
 
 atd = np.matrix([[1, 0, 0, -1],
                  [0, 1, 0, 1],
                  [0, 0, 1, 0],
                  [0, 0, 0, 1]])
 
-ctd = np.matrix([[0, 1, 0, -1],
-                 [0, 1, 0, 1],
+ctd = np.matrix([[0, 1, 0, 0],
+                 [1, 0, 0, 0],
+                 [0, 0, -1, -2],
+                 [0, 0, 0, 1]])
+
+btc = np.matrix([[1, 0, 0, 4],
+                 [0, 1, 0, 0],
                  [0, 0, 1, 0],
                  [0, 0, 0, 1]])
+
+res = atd*inv(ctd)*inv(btc)
+# res = inv(btc)
+print(res)
